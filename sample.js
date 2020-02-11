94e008a65eef6b45a7d281c48a1d98be9fd8a659
Suraj Kumar <ravi.suraj110@gmail.com>
2020-02-11 18:51:14 +0530

    add sample.js

---
+++ sample.js
@@ -0,0 +1,48 @@
+// Sample data
+db.users.insertMany([{ 
+    "age": 49, "name": "Maurice Brock", "email": "wuk@hibpiz.ch", 
+    gender: "Female", sports: ["cricket", "football"],
+    scores: [24, 35, 18, 16], weight: 45
+  },
+  { "age": 37, "birthday": "7/15/1986", "name": "Virgie Cunningham", 
+    "email": "ezogafa@de.gm", "gender": "Male", sports: ['football'], 
+    scores: [17, 35, 43], weight: 52
+  },
+  { "age": 48, "birthday": "5/14/1961", "name": "Alexander Holt", 
+    "email": "han@mu.pe", "gender": "Male", sports: ['cricket', 'football', 'TT'],
+    scores: [24, 30, 16], weight: 55
+  },
+  { "age": 53, "birthday": "11/15/1963", "name": "Derek Dawson", 
+    "email": "polril@now.de", "gender": "Male", sports: ['cricket', 'hockey'], 
+    scores: [20, 15, 38, 23], weight: 49
+  },
+  { "age": 34, "birthday": "7/24/1964", "name": "Cynthia Cobb", 
+    "email": "wujjarpob@jecimpar.gu", "gender": "Female", sports: ['cricket'], 
+    scores: [41, 17, 28], weight: 51
+  },
+  { "age": 33, "birthday": "10/26/1982", "name": "Isabella Atkins", 
+    "email": "ononuzas@givhoz.ca", "gender": "Female", 
+    sports: ['cricket', 'football', 'hockey', 'TT'], scores: [14, 38, 29, 45, 20],
+    weight: 49
+  },
+  { "age": 47, "birthday": "10/12/1978", "name": "Katharine Bryan", 
+    "email": "zo@pebi.sa", "gender": "Male", sports: ['TT', 'hockey', 'khokho'], 
+    scores: [27, 20, 34], weight: 58
+  },
+  { "age": 41, "birthday": "1/28/1991", "name": "Beatrice Fleming", 
+    "email": "ufufsa@pujizren.tk", "gender": "Female", 
+    sports: ['football', 'khokho'], scores: [30, 20, 15, 29, 43], weight: 47
+  },
+  { "age": 26, "birthday": "3/23/1998", "name": "Tom Fields", 
+    "email": "wasodjow@ofaba.gf", "gender": "Female", sports: ['khokho'], 
+    scores: [37, 29, 18, 43, 49], weight: 50
+  },
+  { "age": 33, "birthday": "11/14/1960", "name": "Steve Ortega", 
+    "email": "dupus@ca.ls", "gender": "Female", 
+    sports: ['cricket', 'football', 'hockey'], scores: [12, 15, 20], weight: 51
+  },
+  {
+    age: 24, birthday: "1/5/1994", name: "Suraj Kumar", weight: 50, gender: "Male",
+    sports: ['football', 'cricket', 'TT']
+  }]);
+
